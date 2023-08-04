78ef973199c0110b593db034523a1391ca807128
Drpto <dr.juniornoob@gmail.com>
2023-08-03 23:47:05 -0700

    violet: Go Alpha Official
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -10,6 +10,27 @@ $(call inherit-product, device/xiaomi/violet/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Maintainer
+ALPHA_BUILD_TYPE := Official
+ALPHA_MAINTAINER := Dr.Opto
+
+# Device prop
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Charging Animation
+TARGET_USE_PIXEL_CHARGER := true
+
+#AlphaDroid
+WITH_GAPPS := 2
+TARGET_USE_GOOGLE_TELEPHONY := true
+TARGET_INCLUDE_MATLOG := false
+TARGET_DEFAULT_ADB_ENABLED := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_violet
 PRODUCT_DEVICE := violet
