8ce4ea5a6ac47bf3b3335aa8832a3f3a5765900b
Drpto <dr.juniornoob@gmail.com>
2024-01-12 22:41:20 +0530

    violet: track firmware from https://gitlab.com/crdroidandroid/android-vendor-xiaomi-firmware-violet.git
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@ t clone https://github.com/crdroidandroid/proprietary_vendor_xiaomi_violet.git -
 git clone https://github.com/crdroidandroid/android_kernel_xiaomi_violet.git -b 14.0 kernel/xiaomi/violet --depth=1
 rm -rf prebuilts/clang/host/linux-x86/clang-r510928
 git clone --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r510928.git -b main prebuilts/clang/host/linux-x86/clang-r510928
-git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi-firmware.git -b thirteen vendor/xiaomi-firmware --depth=1
+git clone https://gitlab.com/crdroidandroid/android-vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware --depth=1
 rm -rf packages/resources/devicesettings
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-20.0 packages/resources/devicesettings --depth=1
 rm -rf hardware/qcom-caf/sm8150/audio
